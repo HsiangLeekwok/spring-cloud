240079c24335e8977a98c0ef6e0629288d9d8fcd
Hsiang Leekwok <xfeiffer@hotmail.com>
2020-02-10 20:34:51 +0800

    代码方式的全局 feign 配置

--- src/main/java/com/leekwok/msclass/MsClassApplication.java
+++ src/main/java/com/leekwok/msclass/MsClassApplication.java
@@ -1,5 +1,6 @@
 package com.leekwok.msclass;
 
+import com.leekwok.msclass.feign.GlobalFeignCliengConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -7,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = GlobalFeignCliengConfiguration.class)
 @SpringBootApplication
 public class MsClassApplication {
 
--- src/main/java/com/leekwok/msclass/feign/GlobalFeignCliengConfiguration.java
+++ src/main/java/com/leekwok/msclass/feign/GlobalFeignCliengConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 
 //@Configuration
-public class MsUserFeignCliengConfiguration {
+public class GlobalFeignCliengConfiguration {
 
     @Bean
     public Logger.Level loggerLevel() {
