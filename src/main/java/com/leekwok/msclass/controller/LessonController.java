79222581b0a078fe206986fab6cecee8587d3847
Hsiang Leekwok <xfeiffer@hotmail.com>
2020-02-11 14:49:15 +0800

    使用 resilience4j 的 RateLimiter 进行限流并指定限流被拦截时的 fallback

--- src/main/java/com/leekwok/msclass/controller/LessonController.java
+++ src/main/java/com/leekwok/msclass/controller/LessonController.java
@@ -2,6 +2,9 @@ package com.leekwok.msclass.controller;
 
 import com.leekwok.msclass.entity.Lesson;
 import com.leekwok.msclass.service.LessonService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/lessons")
+// 可以把 RateLimiter 限流放在类上面，也可以放在具体的方法上
+//@RateLimiter(name = "lessonController")
 public class LessonController {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(LessonController.class);
 
     @Autowired
     private LessonService lessonService;
@@ -26,10 +33,21 @@ public class LessonController {
      * 购买指定 id 的课程
      */
     @GetMapping("/buy/{id}")
+    // 限流放到方法上
+    @RateLimiter(name = "buyById", fallbackMethod = "buyByIdFallback")
     public Lesson buyById(@PathVariable Integer id) {
         // 1、根据 id 查询指定的 lesson
         // 2、根据 lesson.id 查询 user_lesson，如果没有
         // 3、如果 user_lesson == null && 用户余额 > lesson.money
         return this.lessonService.buyById(id);
+    }
+
+    /**
+     * 方法名一定要跟 fallbackMethod 中的一样，且返回值和参数列表要跟正常方法的一样
+     */
+    public Lesson buyByIdFallback(Integer id, Throwable throwable) {
+        LOGGER.info("fallback: ", throwable);
+        // 表示从本地缓存中获取
+        return new Lesson();
     }
 }
